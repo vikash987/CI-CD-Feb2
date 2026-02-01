4b0c9d433cb446e29cb4ad1867cba356e82eee3b
vikash987 <40848251+vikash987@users.noreply.github.com>
2026-02-01 09:50:11 +0530

    Create test_calculator.py

---
+++ test_calculator.py
@@ -0,0 +1,12 @@
+from aap.calculator import add,substract
+
+# from aap -> folder 
+# calculator -> file 
+# add, sub (functioin)
+
+def test_add():
+   assert add(2,3) == 5
+
+
+def test_sub():
+   assert substract(5,3) == 2
